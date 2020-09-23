c502c3a275832c03665b5009da87dead040b884a
Flaudz <nico936d@edu.campusvejle.dk>
2020-09-23 10:31:32 +0200

    Added DataBase.cs, Tournement.cs, Sponsor.cs, Employee.cs and Player.cs

---
+++ Classes/DataBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EsportDanmark.Classes
+{
+    public class DataBase
+    {
+        private string connectionString = "Data Source=CV-BB-5992;Initial Catalog=EsportDanmark;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        // Executes the specified query.
+        private DataSet Execute(string query)
+        {
+            DataSet resultSet = new DataSet();
+            using (SqlDataAdapter adapter = new SqlDataAdapter(new SqlCommand(query, new SqlConnection(connectionString))))
+            {
+                adapter.Fill(resultSet);
+            }
+            return resultSet;
+        }
+
+
+        // Add methodes
+
+        // Making a mothod where there will be added a new player in the database table Players
+        public void AddNewPlayer(Player player)
+        {
+            string addNewPlayerQuery =
+                $"INSERT INTO Players (Name, SummonerName, Rank, PhoneNumber, TournermentType) VALUES ('{player.Name}', '{player.Summonername}', {player.Rank}, {player.Phonenumber}, '{player.Tournermenttype}')";
+            Execute(addNewPlayerQuery);
+        }
+
+        // Making a mothod where there will be added a new employee in the database table Employees
+        public void AddNewEmployee(Employee employee)
+        {
+            string addNewEmploeeQuery =
+                $"INSERT INTO Employees (Name, PhoneNumber, Salary, JobType, JudgeLevel) VALUES ('{employee.Name}', {employee.Phonenumber}, {employee.Salary}, '{employee.Jobtype}', {employee.Judgelevel})";
+            Execute(addNewEmploeeQuery);
+        }
+
+        // Making a mothod where there will be added a new sponsor in the database table Sponsorers
+        public void AddNewSponsor(Sponsor sponsor)
+        {
+            string addNewSponsorQuery =
+                $"INSERT INTO Sponsorers (CompanyName, Branche, PlayerId, PlayerName, PlayerSalery) VALUES ('{sponsor.Companyname}', '{sponsor.Branche}', {sponsor.Playerid}, '{sponsor.Playername}, {sponsor.Playersalery}')";
+            Execute(addNewSponsorQuery);
+        }
+
+        // Making a mothod where there will be added a new tournement in the database table Tournements
+        public void AddNewTournement(Tournement tournement)
+        {
+            string addNewTournementQuery =
+                $"INSERT INTO Tournements (TournermentName, PlayerId, PlayerName, PlayersPhoneNumber, RefId, RefName, RefPhoneNumber, RefLevel) VALUES ('{tournement.Tournermentname}', {tournement.Playerid}, '{tournement.Playername}', {tournement.Playerphonenumber}, {tournement.Refid}, '{tournement.Refname}', {tournement.Refphonenumber}, {tournement.Reflevel})";
+            Execute(addNewTournementQuery);
+        }
+    }
+}
---
+++ Classes/Sponsor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EsportDanmark.Classes
+{
+    public class Sponsor
+    {
+        // Fields
+        private int id;
+        private string companyname;
+        private string branche;
+        private int playerid;
+        private string playername;
+        private int playersalery;
+
+
+        // Properties
+        public int Id { get => id; }
+        public string Companyname { get => companyname; set => companyname = value; }
+        public string Branche { get => branche; set => branche = value; }
+        public int Playerid { get => playerid; set => playerid = value; }
+        public string Playername { get => playername; set => playername = value; }
+        public int Playersalery { get => playersalery; set => playersalery = value; }
+
+
+        // Constructor
+        public Sponsor(string companyname, string branche, int playerid, string playername, int playersalery)
+        {
+            Companyname = companyname;
+            Branche = branche;
+            Playerid = playerid;
+            Playername = playername;
+            Playersalery = playersalery;
+        }
+
+        public Sponsor()
+        {
+
+        }
+    }
+}
