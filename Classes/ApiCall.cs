90fe27b123e999e186ebdee82e27730d9ecbef50
Flaudz <nico936d@edu.campusvejle.dk>
2020-09-24 14:38:24 +0200

    Added Link

--- Classes/ApiCall.cs
+++ Classes/ApiCall.cs
@@ -11,6 +11,8 @@ namespace EsportDanmark.Classes
     {
         public Root GetPlayer(string summoner)
         {
+            // Hvis api nøglen er udløbet så forny den på dette link
+            // https://developer.riotgames.com/
             string json = new WebClient().DownloadString(@$"https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner}?api_key=RGAPI-0669815a-beac-4a33-b695-de56d9e3c8a3");
             Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(json);
             return myDeserializedClass;
